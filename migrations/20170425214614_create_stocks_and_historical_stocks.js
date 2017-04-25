475bb4f5c5cfd6b576066ddb367c476fc1c6ff24
aun <aun.sira@gmail.com>
2017-04-25 22:41:20 +0700

    Add migration file.

---
+++ migrations/20170425214614_create_stocks_and_historical_stocks.js
@@ -0,0 +1,26 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTableIfNotExists('stocks', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.timestamps();
+    }).then(() => {
+      console.log("Created table `stocks`")
+      return knex.schema.createTableIfNotExists('historical_stocks', function(table) {
+        table.increments('id').primary();
+        table.integer('stock_id').unsigned().references('id').inTable('stocks');
+        table.float('value');
+        table.bigint('record_time');
+      });
+    }).then(() => {
+      console.log("Created table `historical_stocks`");
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('stocks'),
+    knex.schema.dropTable('historical_stocks'),
+  ])
+};
