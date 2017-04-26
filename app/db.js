697ca2f9ff9e871769e5dc5f91e2187a50d58e50
aun <aun.sira@gmail.com>
2017-04-26 14:25:31 +0700

    Avoid `bookshelf.js`.

--- app/db.js
+++ app/db.js
@@ -1,8 +1,7 @@
 var config    = require('../knexfile.js');
 var env       = 'development';
 var knex      = require('knex')(config[env]);
-var bookshelf = require('bookshelf')(knex);
 
-module.exports = bookshelf;
+module.exports = knex;
 
 knex.migrate.latest([config]);
